89c262fccaa25353d7fb968f633bc28be2b2cba4
Shogo Komatsu <49989073+ShougoKomatsu@users.noreply.github.com>
2023-06-12 23:51:36 +0900

    リポジトリ分離

--- SAutomation/Common.h
+++
@@ -1,7 +0,0 @@
-#include "stdafx.h"
-
-BOOL GetFileName(CString sFilePath, CString* sFileName);
-
-
-BOOL ReadUTFFile(CString sFilePath, CString* sData);
-BOOL ReadTextFile(CString sFilePath, CStringArray* saCommands);
--- SAutomation/Perser.h
+++
@@ -1,8 +0,0 @@
-#include "stdafx.h"
-#include "MouseAutomation.h"
-
-BOOL GetCommand(CString sDataLine, int* iCommandType);
-
-BOOL ExtractData(const CString sInput, const CString sDelim, CString* sOut, CString* sRemin);
-
-BOOL PerseCommand(int* iSceneData, CString sDataLine, int* iCommandType, CStringArray* saData);
